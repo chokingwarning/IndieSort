a994055d978cdc81078a89181ee5cc4bfe25c571
chokingwarning <cropcircletruther@gmail.com>
2025-03-27 21:28:43 -0400

    Initial commit but correct this time

---
+++ src/js/data/2025-03-27.js
@@ -0,0 +1,28 @@
+dataSetVersion = "2025-03-27"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Remove Mint",
+    key: "mint",
+    tooltip: "Check this to remove mint"
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Mint Fantôme",
+    img: "SurAwkF",
+    opts: { mint: true }
+  },
+  {
+    name: "Nimi Nightmare",
+    img: "CvS9sy2",
+    opts: { }
+  },
+  {
+    name: "Dooby",
+    img: "xJDpJhC",
+    opts: { }
+  }
+];
